1f0d195b15ad9db47d66aea2afb26d59b833e09f
IshmamF <ishmamf2003@gmail.com>
2024-09-30 02:36:44 -0400

    fixed problem1 to write XYZ after evry 100 char

--- homework-1/problem1.cpp
+++ homework-1/problem1.cpp
@@ -8,10 +8,14 @@ using namespace std;
 
 extern int errno;
 
-void replace_and_write(char *buffer, int size, int dest_fd) {
-    for (int i = 0; i < size; i++) {
-        if (isalnum(buffer[i])) {
-            if (buffer[i] == '1') {
+void replace_and_write(char *buffer, int size, int dest_fd)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (isalnum(buffer[i]))
+        {
+            if (buffer[i] == '1')
+            {
                 buffer[i] = 'A';
             }
             write(dest_fd, &buffer[i], 1);
@@ -19,10 +23,10 @@ void replace_and_write(char *buffer, int size, int dest_fd) {
     }
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argc != 4) {
+    if (argc != 4)
+    {
         printf("Use the following: %s directoryNameWithPath source.txt newfile.txt\n", argv[0]);
         exit(1);
     }
@@ -31,7 +35,8 @@ int main(int argc, char* argv[])
     char *source_file = argv[2];
     char *new_file = argv[3];
 
-    if (mkdir(dir_path, 0777) == -1) {
+    if (mkdir(dir_path, 0777) == -1)
+    {
         printf("%s", "Error creating directory");
         exit(1);
     }
@@ -41,14 +46,16 @@ int main(int argc, char* argv[])
 
     // Open source file
     int source_fd = open(source_file, O_RDONLY);
-    if (source_fd == -1) {
+    if (source_fd == -1)
+    {
         perror("Error opening source file");
         exit(1);
     }
 
     // Create and open new file
     int dest_fd = open(full_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dest_fd == -1) {
+    if (dest_fd == -1)
+    {
         perror("Error creating new file");
         close(source_fd);
         exit(1);
@@ -56,7 +63,6 @@ int main(int argc, char* argv[])
 
     int bytesRead;
     char buffer[101];
-
 
     /*
     bytesRead = read(source_fd, buffer, 100);
@@ -67,21 +73,24 @@ int main(int argc, char* argv[])
         return 1;
     }*/
 
-    while ((bytesRead = read(source_fd, buffer, 100)) > 0) {
-        buffer[bytesRead] = '\0';  // Null-terminate the string
+    while ((bytesRead = read(source_fd, buffer, 100)) > 0)
+    {
+        buffer[bytesRead] = '\0'; // Null-terminate the string
 
-        for (int i = 0; i < bytesRead; i++) {
-            if (isalnum(buffer[i])) {
-                if (buffer[i] == '1') {
+        for (int i = 0; i < bytesRead; i++)
+        {
+            if (isalnum(buffer[i]))
+            {
+                if (buffer[i] == '1')
+                {
                     buffer[i] = 'A';
                 }
             }
             write(dest_fd, &buffer[i], 1);
         }
+        write(dest_fd, "XYZ", 3);
     }
 
-
-    write(dest_fd, "XYZ", 3);
     buffer[bytesRead] = '\0';
     printf("%s", buffer);
     close(source_fd);
